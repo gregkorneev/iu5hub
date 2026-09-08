782e6c60b1c375cf304a6f3ee4ddb6f6f5510c9a
gregkorneev <gregory.korneev@gmail.com>
2026-09-08 22:03:21 +0300

    feat: expand course navigation and layout

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { initializePlatform } from './platform'
 import './styles.css'
+import './responsive.css'
 
 initializePlatform()
 createRoot(document.getElementById('root')!).render(<StrictMode><BrowserRouter><App /></BrowserRouter></StrictMode>)
